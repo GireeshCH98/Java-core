7f1f20f06426b071dbb78ac90eef5c8862b53ab5
Suhana R Ghasi <suhanarg.mca19@rvce.edu.in>
2024-03-06 01:08:02 +0530

    Added Succesfully

--- src/com/learning/core/day3/D03P13.java
+++ src/com/learning/core/day3/D03P13.java
@@ -4,14 +4,47 @@ import java.util.Scanner;
 
 public class D03P13 
 {
-	 public static void main(String[] args) 
+	/* public static void main(String[] args) 
 	    {
-	        //String str = "Mr John Smith   ";
-	       Scanner sr = new Scanner(System.in); 
-		    System.out.println("Enter a String:");
-		    String str = sr.next();
+	        String str = "Mr John Smith   ";
+	        System.out.println(input);
 	        str = str.trim();
-	        str = str.replaceAll(" ", "%20");
+	        str = str.replaceAll("\\s", "%20");
 	        System.out.println(str);
-	    }
+	    } */
+	public static String replaceSpaces(String str) 
+	{
+		char[] charArray = str.toCharArray();
+		int spaceCount = 0;
+        for (char c : charArray) {
+            if (c == ' ') {
+                spaceCount++;
+            }
+        }
+        int newLength = str.length() + (spaceCount * 2);
+        char[] newCharArray = new char[newLength];
+        
+        int index = 0;
+        for (char c : charArray) {
+            if (c == ' ') {
+                newCharArray[index++] = '%';
+                newCharArray[index++] = '2';
+                newCharArray[index++] = '0';
+            } 
+            else 
+            {
+                newCharArray[index++] = c;
+            }
+        }
+
+        return new String(newCharArray);
+    }
+
+    public static void main(String[] args) {
+        String input = "Mr John Smith";
+        System.out.println(input);
+        String output = replaceSpaces(input);
+        System.out.println(output);
+    }
+
 }
