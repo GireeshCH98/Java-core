2b376d5b965baf3f64339cb189d66fb6b7808819
Gireesh C Hosakoppa <83752497+GireeshCH98@users.noreply.github.com>
2024-03-08 00:38:11 +0530

    Add files via upload

---
+++ src/com/learning/core/day5/D05P03.java
@@ -0,0 +1,68 @@
+package com.learning.core.day5;
+
+import java.util.LinkedHashSet;
+class Product 
+{
+    private String productId;
+    private String productName;
+
+    public Product(String productId, String productName) 
+    {
+    	this.productId = productId;
+    	this.productName = productName;
+    }
+
+	public String getProductId() 
+	{
+		return productId;
+	}
+
+	public String getProductName() 
+	{
+		return productName;
+	}
+	@Override
+	public String toString() 
+	{
+		return productId + " " + productName;
+	}
+	@Override
+	public int hashCode() 
+	{
+		return productId.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		Product product = (Product) obj;
+		return productId.equals(product.productId);
+	}
+}
+public class D05P03 
+{
+	public static void main(String[] args) 
+	{
+		LinkedHashSet<Product> productSet = new LinkedHashSet<>();
+		//I have Predefined information of 4 products
+		Product product1 = new Product("P001", "Maruti 800");
+	    Product product2 = new Product("P002", "Maruti Zen");
+	    Product product3 = new Product("P003", "Maruti Dezire");
+	    Product product4 = new Product("P004", "Maruti Alto");
+
+	    // Adding products to the HashSet
+	    productSet.add(product1);
+	    productSet.add(product2);
+	    productSet.add(product3);
+	    productSet.add(product4);
+
+	    // Displaying product details
+	    for (Product product : productSet) 
+	    {
+	    	System.out.println(product);
+	    }
+	}
+}
