93ec987f4581fcce78bad75ca89f6d6389507d48
Gireesh C Hosakoppa <83752497+GireeshCH98@users.noreply.github.com>
2024-03-11 15:27:29 +0530

    Add files via upload

---
+++ src/com/learning/core/day6/D06P02.java
@@ -0,0 +1,37 @@
+package com.learning.core.day6;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class D06P02
+{
+	public static void main(String[] args) 
+	{
+		// Creating a HashMap to store phone book details
+	    Map<String, String> phoneBook = new HashMap<>();
+
+	    // Adding predefined phone book details
+	    phoneBook.put("Amal", "998787823");
+	    phoneBook.put("Manvitha", "937843978");
+	    phoneBook.put("Joseph", "7882221113");
+	    phoneBook.put("Smith", "8293893311");
+	    phoneBook.put("Kathe", "7234560011");
+
+	    // Name to search
+	    String searchName = "Joseph";
+
+	    // Searching for the phone number
+	    String phoneNumber = phoneBook.get(searchName);
+
+	    // Outputting the phone number if found
+	    if (phoneNumber != null) 
+	    {
+	    	System.out.println(phoneNumber);
+	    } 
+	    else 
+	    {
+	    	System.out.println("No phone number found for " + searchName);
+	    }
+	}
+}
+
