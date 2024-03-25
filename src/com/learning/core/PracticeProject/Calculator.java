6a2d977c2ffacbd7f4d34482b532afc3c64980d1
Gireesh C Hosakoppa <83752497+GireeshCH98@users.noreply.github.com>
2024-03-25 14:50:43 +0530

    Add files via upload

---
+++ src/com/learning/core/PracticeProject/Calculator.java
@@ -0,0 +1,24 @@
+package com.learning.core.PracticeProject;
+
+public class Calculator {
+	// Method to add two numbers
+	static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    // Method to subtract two numbers
+	static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    // Method to multiply two numbers
+    static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    // Method to divide two numbers
+    static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
+
